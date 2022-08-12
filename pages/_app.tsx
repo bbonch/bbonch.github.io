d442b730ffac09479dc3c7122b88383562dee46f
Dmitry Sergienko <dmitry.sergienko@codefirst.net>
2022-08-12 20:58:07 +0400

    message=Fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from 'next/app'
 import '../styles/sfd.scss'
 import Script from 'next/script';
+import Head from 'next/head'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="author" content="Dmitry Sergienko" />
+        <meta property="og:image" content="logo.png" />
+      </Head>
+
       <Component {...pageProps} />
 
       <Script src="https://www.googletagmanager.com/gtag/js?id=UA-50788406-5" strategy="afterInteractive" />
